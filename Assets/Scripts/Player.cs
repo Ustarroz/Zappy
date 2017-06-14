7ba72070e174cc075cd5bf1e076afd7ce82afdb6
Mathieu Sauvau <Mathieu Sauvau>
2017-06-14 15:37:46 +0200

    map

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamelogic.Grids2;
+
+public class Player : MonoBehaviour
+{
+    Grid3<MeshTileCell> grid;
+    GridPoint3 gridPos;
+    Vector3 pos;
+
+    int Level { get; set; }
+    int Food { get; set; }
+    int Linemate { get; set; }
+    int Deraumere { get; set; }
+    int Sibur { get; set; }
+    int Mendiane { get; set; }
+    int Phiras { get; set; }
+    int Thystame { get; set; }
+
+    void spawn()
+    {
+
+    }
+
+    void move()
+    {
+
+    }
+
+}
