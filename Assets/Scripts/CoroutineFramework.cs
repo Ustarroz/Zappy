b48b758530dc87d9635386c764c3c0a66d2c0af8
Mathieu Sauvau <Mathieu Sauvau>
2017-06-21 09:25:24 +0200

    opps skybox

---
+++ Assets/Scripts/CoroutineFramework.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineFramework : MonoBehaviour
+{
+
+    private List<string> runningCoroutinesByStringName = new List<string>();
+    private List<IEnumerator> runningCoroutinesByEnumerator = new List<IEnumerator>();
+
+    public Coroutine StartTrackedCoroutine(string methodName)
+    {
+        return StartCoroutine(GenericRoutine(methodName, null));
+    }
+
+    public Coroutine StartTrackedCoroutine(IEnumerator coroutine)
+    {
+        return StartCoroutine(GenericRoutine(coroutine));
+    }
+
+    public Coroutine StartTrackedCoroutine(string methodName, object parameter)
+    {
+        return StartCoroutine(GenericRoutine(methodName, parameter));
+    }
+
+    public bool IsTrackedCoroutineRunning(string methodName)
+    {
+        return runningCoroutinesByStringName.Contains(methodName);
+    }
+
+    public bool IsTrackedCoroutineRunning(IEnumerator coroutine)
+    {
+        return runningCoroutinesByEnumerator.Contains(coroutine);
+    }
+
+    public bool IsTrackedCoroutineRunning()
+    {
+        return runningCoroutinesByEnumerator.Count > 0;
+    }
+
+    public void StopTrackedCoroutine(string methodName)
+    {
+        if (!runningCoroutinesByStringName.Contains(methodName))
+        {
+            return;
+        }
+        StopCoroutine(methodName);
+        runningCoroutinesByStringName.Remove(methodName);
+    }
+
+    public void StopTrackedCoroutine(IEnumerator coroutine)
+    {
+        if (!runningCoroutinesByEnumerator.Contains(coroutine))
+        {
+            return;
+        }
+        StopCoroutine(coroutine);
+        runningCoroutinesByEnumerator.Remove(coroutine);
+    }
+
+    private IEnumerator GenericRoutine(string methodName, object parameter)
+    {
+        runningCoroutinesByStringName.Add(methodName);
+        if (parameter == null)
+        {
+            yield return StartCoroutine(methodName);
+        }
+        else
+        {
+            yield return StartCoroutine(methodName, parameter);
+        }
+        runningCoroutinesByStringName.Remove(methodName);
+    }
+
+    private IEnumerator GenericRoutine(IEnumerator coroutine)
+    {
+        runningCoroutinesByEnumerator.Add(coroutine);
+        yield return StartCoroutine(coroutine);
+        runningCoroutinesByEnumerator.Remove(coroutine);
+    }
+}
---
+++ Assets/Scripts/ZappyObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZappyObjects {
+
+    int Food { get; set; }
+    int Linemate { get; set; }
+    int Deraumere { get; set; }
+    int Sibur { get; set; }
+    int Mendiane { get; set; }
+    int Phiras { get; set; }
+    int Thystame { get; set; }
+
+    public ZappyObjects()
+    {
+        Food = Linemate = Deraumere = Sibur = Mendiane = Phiras = Thystame = 0;
+    }
+}
