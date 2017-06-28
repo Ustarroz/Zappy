6aefd7db6eff912ac957bc6867b700cc8163f1b0
Mathieu Sauvau <Mathieu Sauvau>
2017-06-28 17:49:12 +0200

    fix merge tcpc

--- Assets/Scripts/TcpClientAsync.cs
+++ Assets/Scripts/TcpClientAsync.cs
@@ -4,167 +4,149 @@ using System.Text;
 using System.Reflection;
 using UnityEngine;
 
-<<<<<<< HEAD
-namespace TcpAsync
+
+public class TcpClientAsync
 {
-=======
->>>>>>> d5236a8fc0b39389ad449ab24a1f949850988b1f
-    public class TcpClientAsync
+    private TcpClient client = null;
+    private string returndata = String.Empty;
+    public bool connectStatus = false;
+    public bool receiveStatus = false;
+    public bool sendStatus = false;
+    public bool disconnectStatus = false;
+    public const int BufferSize = 4096;
+    private byte[] buffer = new byte[BufferSize];
+    private string sendMsg = String.Empty;
+    private int port;
+    private string ip;
+
+    // Constructor
+    public TcpClientAsync()
     {
-        private TcpClient client = null;
-        private string returndata = String.Empty;
-        public bool connectStatus = false;
-        public bool receiveStatus = false;
-        public bool sendStatus = false;
-        public bool disconnectStatus = false;
-        public const int BufferSize = 4096;
-        private byte[] buffer = new byte[BufferSize];
-        private string sendMsg = String.Empty;
-        private int port;
-        private string ip;
+        client = new TcpClient();
+    }
 
-        // Constructor
-        public TcpClientAsync()
+    public void Connect(string _ip, int _port)
+    {
+        try
         {
-            client = new TcpClient();
+            ip = _ip;
+            port = _port;
+            connectStatus = false;
+            if (client.Connected)
+                Disconnect();
+            client.Client.BeginConnect(ip, port, new AsyncCallback(ConnectCallback), null);
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
 
-<<<<<<< HEAD
-        #region Connect Functions
-=======
->>>>>>> d5236a8fc0b39389ad449ab24a1f949850988b1f
-        public void Connect(string _ip, int _port)
-        {
-            try
-            {
-                ip = _ip;
-                port = _port;
-                connectStatus = false;
-                if (client.Connected)
-                    Disconnect();
-                client.Client.BeginConnect(ip, port, new AsyncCallback(ConnectCallback), null);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
+    public void ConnectCallback(IAsyncResult ar)
+    {
+        client.EndConnect(ar);
+        client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+        connectStatus = true;
+    }
 
-        public void ConnectCallback(IAsyncResult ar)
+    public void Receive()
+    {
+        try
         {
-            client.EndConnect(ar);
-            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
-            connectStatus = true;
-        }
-        
-        public void Receive()
-        {
-            try
+            if (client.Connected)
             {
-                if (client.Connected)
-                {
-                    returndata = String.Empty;
-                    receiveStatus = false;
-                    buffer = new byte[BufferSize];
-                    client.Client.BeginReceive(buffer, 0, BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-        private void ReceiveCallback(IAsyncResult ar)
-        {
-            int bytesRead = client.GetStream().EndRead(ar);
-            if (bytesRead > 0)
-            {
-                returndata += Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                returndata = String.Empty;
+                receiveStatus = false;
                 buffer = new byte[BufferSize];
-                if (client.Client.Poll(0, SelectMode.SelectRead) || returndata[returndata.Length - 1] != '\n')
-                    client.Client.BeginReceive(buffer, 0, BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
-                else
-                {
-                    receiveStatus = true;
-                }
+                client.Client.BeginReceive(buffer, 0, BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
             }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private void ReceiveCallback(IAsyncResult ar)
+    {
+        int bytesRead = client.GetStream().EndRead(ar);
+        if (bytesRead > 0)
+        {
+            returndata += Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            buffer = new byte[BufferSize];
+            if (client.Client.Poll(0, SelectMode.SelectRead) || returndata[returndata.Length - 1] != '\n')
+                client.Client.BeginReceive(buffer, 0, BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
             else
             {
-                if (returndata.Length > 1)
-                    returndata += Encoding.ASCII.GetString(buffer, 0, bytesRead);
                 receiveStatus = true;
             }
         }
-
-        public void Send(string data)
+        else
         {
-            if (client.Connected)
-            {
-                byte[] byteData = Encoding.ASCII.GetBytes(data);
-                sendStatus = false;
-                client.Client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), null);
-                sendMsg = data;
-            }
+            if (returndata.Length > 1)
+                returndata += Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            receiveStatus = true;
         }
+    }
 
-        private void SendCallback(IAsyncResult ar)
+    public void Send(string data)
+    {
+        if (client.Connected)
         {
-            try
-            {
-                int bytesSent = client.Client.EndSend(ar);
-                sendStatus = true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            sendStatus = false;
+            client.Client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), null);
+            sendMsg = data;
         }
+    }
 
-        public void Disconnect()
+    private void SendCallback(IAsyncResult ar)
+    {
+        try
         {
-            if (client.Connected)
-            {
-                client.GetStream().Close();
-                client.Close();
-                connectStatus = false;
-                receiveStatus = false;
-                sendStatus = false;
-            }
+            int bytesSent = client.Client.EndSend(ar);
+            sendStatus = true;
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
 
-<<<<<<< HEAD
-        #region Getter
-=======
->>>>>>> d5236a8fc0b39389ad449ab24a1f949850988b1f
-        public string getData()
+    public void Disconnect()
+    {
+        if (client.Connected)
         {
-            return returndata;
+            client.GetStream().Close();
+            client.Close();
+            connectStatus = false;
+            receiveStatus = false;
+            sendStatus = false;
         }
+    }
 
-        public string getSend()
-        {
-            return sendMsg;
-        }
+    public string getData()
+    {
+        return returndata;
+    }
 
-        public string IP()
-        {
-            return ip;
-        }
+    public string getSend()
+    {
+        return sendMsg;
+    }
 
-        public int Port()
-        {
-            return port;
-        }
+    public string IP()
+    {
+        return ip;
+    }
 
-        public Socket getSocket()
-        {
-            return client.Client;
-        }
-<<<<<<< HEAD
-        #endregion
+    public int Port()
+    {
+        return port;
+    }
+
+    public Socket getSocket()
+    {
+        return client.Client;
     }
 }
-=======
-    }
->>>>>>> d5236a8fc0b39389ad449ab24a1f949850988b1f
