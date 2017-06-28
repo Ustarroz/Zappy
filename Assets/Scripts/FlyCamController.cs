1eed544854d27378128c3b35a164b5c6c12bc827
bougon_p-PC\bougon_p <pierre.bougon@epitech.eu>
2017-06-28 16:10:19 +0200

    Add button to enable/disable flycam

--- Assets/Scripts/FlyCamController.cs
+++ Assets/Scripts/FlyCamController.cs
@@ -16,47 +16,52 @@ namespace UnityStandardAssets.Characters.FirstPerson
         float camSens = 0.20f; //How sensitive it with mouse
         private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
         private float totalRun= 1.0f;
+        private bool flyMode = false;
 
         void Update () 
         {
-            lastMouse = Input.mousePosition - lastMouse ;
-            lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
-            lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
-            transform.eulerAngles = lastMouse;
-            lastMouse =  Input.mousePosition;
-            //Mouse  camera angle done.  
-        
-            //Keyboard commands
-            float f = 0.0f;
-            Vector3 p = GetBaseInput();
-            if (Input.GetKey (KeyCode.LeftShift))
+            if (flyMode)
             {
-                totalRun += Time.deltaTime;
-                p  = p * totalRun * shiftAdd;
-                p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-                p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-                p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-            }
-            else
-            {
-                totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-                p = p * mainSpeed;
-            }
+                lastMouse = Input.mousePosition - lastMouse ;
+                lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
+                lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
+                transform.eulerAngles = lastMouse;
+                lastMouse =  Input.mousePosition;
+                //Mouse  camera angle done.  
         
-            p = p * Time.deltaTime;
-            Vector3 newPosition = transform.position;
-            if (Input.GetKey(KeyCode.Space))
-            { //If player wants to move on X and Z axis only
-                transform.Translate(p);
-                newPosition.x = transform.position.x;
-                newPosition.z = transform.position.z;
-                transform.position = newPosition;
-            }
-            else
-            {
-                transform.Translate(p);
-            }
-        
+                //Keyboard commands
+                float f = 0.0f;
+                Vector3 p = GetBaseInput();
+                if (Input.GetKey (KeyCode.LeftShift))
+                {
+                    totalRun += Time.deltaTime;
+                    p  = p * totalRun * shiftAdd;
+                    p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+                    p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+                    p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+                }
+                else
+                {
+                    totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+                    p = p * mainSpeed;
+                }
+            
+                p = p * Time.deltaTime;
+                Vector3 newPosition = transform.position;
+                if (Input.GetKey(KeyCode.Space))
+                { //If player wants to move on X and Z axis only
+                    transform.Translate(p);
+                    newPosition.x = transform.position.x;
+                    newPosition.z = transform.position.z;
+                    transform.position = newPosition;
+                }
+                else
+                {
+                    transform.Translate(p);
+                }
+            }   
+            if (Input.GetKeyDown (KeyCode.F))
+                flyMode = !flyMode;
         }
         
         private Vector3 GetBaseInput() 
