97111d7be722f965f51e95ace2d8d682d71a2d67
bougon_p-PC\bougon_p <pierre.bougon@epitech.eu>
2017-07-01 00:20:20 +0200

    egg sound ok

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -4,20 +4,49 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class Egg : MonoBehaviour
 {
+    private Vector2 position;
+    private int     id;
+
     // Audio
+    public float       volume;
     public AudioClip   eggPopSound;
     public AudioClip   eggCrackSound;
     public AudioSource audio;
 
-    public void Pop(Vector3 playerPos)
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void Pop()
     {
         audio.Stop();
-        audio.PlayOneShot(eggPopSound, 1);
+        audio.PlayOneShot(eggPopSound, volume);
     }
 
     public void Crack()
     {
         audio.Stop();
-        audio.PlayOneShot(eggCrackSound, 1);
+        audio.PlayOneShot(eggCrackSound, volume);
+    }
+
+    public Vector2 getPosition()
+    {
+        return position;
+    }
+
+    public void setPosition(Vector2 _pos)
+    {
+        position = _pos;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int _id)
+    {
+        id = _id;
     }
 }
