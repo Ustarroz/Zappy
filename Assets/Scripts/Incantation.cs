9389b6e7813f72a42fda58336292d2129b222f07
bougon_p-PC\bougon_p <pierre.bougon@epitech.eu>
2017-06-30 03:35:35 +0200

    particle added

--- Assets/Scripts/Incantation.cs
+++ Assets/Scripts/Incantation.cs
@@ -2,25 +2,39 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(ParticleSystem))]
 public class Incantation : MonoBehaviour
 {
+    // Audio
     public AudioClip   incantationMainSound;
     public AudioClip   incantationEndSound;
     public AudioClip   incantationLevelUpSound;
     public AudioSource audio;
 
+    // Particle
+    public ParticleSystem particle;
+
     public void PlayIncantation()
     {
         audio.Stop();
-        audio.PlayOneShot(incantationMainSound, 1f);        
+        audio.PlayOneShot(incantationMainSound, 1f);
+        particle.Stop();
+        particle.Play();
+        StopIncantation(true);  
     }
 
     public void StopIncantation(bool success)
     {
         audio.Stop();
         if (success)
+        {
+            particle.Stop();
             audio.PlayOneShot(incantationLevelUpSound, 0.5f);
+        }
         else
+        {
+            particle.Stop();
             audio.PlayOneShot(incantationEndSound, 0.5f);
+        }
     }
 }
